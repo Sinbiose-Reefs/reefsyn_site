b23b8078689a48a53bf91321361a30795627bbab
andreluza <luza.andre@gmail.com>
2020-08-25 19:00:06 -0300

    Building the reefsyn website

---
+++ R/packages.R
@@ -0,0 +1,15 @@
+## pacotes para abrir dados
+require (here)
+require (openxlsx)
+require(rgdal)
+require(raster)
+
+## pacotes para mapas
+require(rnaturalearth)
+require(rnaturalearthdata)
+require(ggplot2)
+require(gridExtra)
+require(ggrepel)
+require ("scatterpie")
+require(sf)
+require(rgeos)
